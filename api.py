51d4ab7a650a7a3e61c9bbdb1d7c2c3c82dbf0db
Dima <random1k11@gmail.com>
2019-09-25 23:22:09 +0300

    Refactoring

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from webob import Request, Response
+from parse import parse
 
 
 class API:
@@ -12,17 +13,20 @@ class API:
             return handler
         return wrapper
 
-    def __call__(self, environ, start_response):
-        request = Request(environ)
-        response = Response()
-        return response(environ, start_response)
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            print(path, request_path)
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                print(parse_result.named)
+                return handler, parse_result.named
+        return None, None
 
     def handle_request(self, request):
         response = Response()
-        handler = self.find_handler(request_path=request.path)
-        print(request.path)
+        handler, kwargs = self.find_handler(request_path=request.path)
         if handler is not None:
-            handler(request, response)
+            handler(request, response, **kwargs)
         else:
             self.default_response(response)
         return response
@@ -31,8 +35,7 @@ class API:
         response.status_code = 404
         response.text = "Not found."
 
-    def find_handler(self, request_path):
-        for path, handler in self.routes.items():
-            if path == request_path:
-                return handler
-
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
